951bc11ffbd4e41e8cb356749a4cd1ec8523e4bb
satyabrata biswal <86743992+saty4a@users.noreply.github.com>
2024-02-01 00:49:13 +0530

    Update socketConnection.jsx

--- task-management-frontend/src/config/socketConnection.jsx
+++ task-management-frontend/src/config/socketConnection.jsx
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io.connect("http://localhost:4000");
+export const socket = io.connect("https://task-management-blue-theta.vercel.app");
